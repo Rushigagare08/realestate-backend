f5bce1a55f10e0fa75cd05cc68199b313b42512e
Rushigagare08 <154578666+Rushigagare08@users.noreply.github.com>
2026-02-18 16:29:40 +0530

    Fix import case for Oauth component

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import Oauth from "../components/Oauth";
+import Oauth from "../components/OAuth";
 
 export default function SignUp() {
   const navigate = useNavigate();
